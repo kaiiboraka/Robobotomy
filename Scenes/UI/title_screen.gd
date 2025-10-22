8d32fdd89492ec9b34e7af1cc186ec86c93c9a73
TheNewGrant <masoncgrant@gmail.com>
2025-10-22 09:05:25 -0600

    Can specify level to load
    In the Loading Screen, you can now add in an optional scene file path to load the specific level scene.

--- Scenes/UI/title_screen.gd
+++ Scenes/UI/title_screen.gd
@@ -3,6 +3,9 @@ class_name TitleScreen
 
 
 func _on_button_pressed() -> void:
-	LevelManager.load_level("res://Scenes/sandbox_levels/sb_1.tscn")
+	var level_to_load = %SceneToLoad.text
+	if level_to_load == "":
+		level_to_load = "res://Scenes/sandbox_levels/sb_1.tscn"
+	LevelManager.load_level(level_to_load)
 	self.hide()
 	%Button.disabled = true
