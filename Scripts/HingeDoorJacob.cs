e96165876258f42867980920c18b4d6e046ca353
Jacob <jmemmott13@gmail.com>
2025-10-24 16:10:47 -0600

    add: my sandbox level, empty HingeDoor class

---
+++ Scripts/HingeDoorJacob.cs
@@ -0,0 +1,19 @@
+using Godot;
+using System;
+
+public partial class HingeDoorJacob : Node3D
+{
+	[Export]
+	public float doorHeight;
+
+	MeshInstance3D hingeDoorMesh = ;
+	// questions to answer:
+	// how do I procedurally create a mesh?
+	// how do I get a reference to 'Hinge Door Mesh' in the same way the Rope does?
+	// 
+	private void _update_hinge_door_geometry()
+	{
+		CylinderMesh hingeDoorMesh = hingeDoorMesh.mesh;
+		//hingeDoorMesh.set_height(doorHeight);
+	}
+}
