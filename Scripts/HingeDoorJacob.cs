ffa0582743a52d3c8ed5a1c7454a236712e58171
Jacob <jmemmott13@gmail.com>
2025-11-07 17:05:24 -0700

    add: [Tool] marking and _Process() so the door length updates in the editor

--- Scripts/HingeDoorJacob.cs
+++ Scripts/HingeDoorJacob.cs
@@ -1,24 +1,31 @@
 using Godot;
 using System;
 
+[Tool]
 public partial class HingeDoorJacob : Node3D
 {
-    [Export]
-    public float doorHeight;
+	[Export]
+	public float doorHeight;
 
-    [Export]
-    private MeshInstance3D hingeDoorMesh;
-    // questions to answer:
-    // how do I procedurally create a mesh?
-    // how do I get a reference to 'Hinge Door Mesh' in the same way the Rope does?
-    // 
-    private void _UpdateHingeDoorGeometry()
-    {
-        var doorMesh = hingeDoorMesh.Mesh as CylinderMesh;
-        doorMesh.Height = doorHeight;
-    }
-    public override void _Ready()
-    {
-        _UpdateHingeDoorGeometry();
-    }
+	[Export]
+	private MeshInstance3D hingeDoorMesh;
+	[Export]
+	private CollisionShape3D doorCollisionShape;
+	
+	private void _UpdateHingeDoorGeometry()
+	{
+		var doorMesh = hingeDoorMesh.Mesh as CylinderMesh;
+		doorMesh.Height = doorHeight;
+		hingeDoorMesh.Position = new Vector3(0, doorHeight/2, 0);
+		var collisionMesh = doorCollisionShape.Shape as CylinderShape3D;
+		collisionMesh.Height = doorHeight;
+		doorCollisionShape.Position = new Vector3(0, doorHeight/2, 0);
+	}
+	public override void _Ready()
+	{		
+		_UpdateHingeDoorGeometry();
+	}
+	public override void _Process(double delta) {
+		_UpdateHingeDoorGeometry();
+	}
 }
