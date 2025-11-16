c4d347ab4a2128f8daa228337adea5a2ba95dc6b
Chase Odom <chasemichaelodom@gmail.com>
2025-11-16 15:03:18 -0700

    Adding code for lightGrid generation

--- Scripts/light_grid.gd
+++ Scripts/light_grid.gd
@@ -16,31 +16,34 @@ const light_scene := preload("uid://dx8q4j1vvqxrn")
 		return light_width
 	set(value):
 		light_width = clamp(value,MIN_LIGHT_WIDTH_OR_HEIGHT,MAX_LIGHT_WIDTH_OR_HEIGHT)
-		generate_grid()
-
+		_generate_grid()
 @export var light_height : int = 4:
 	get:
 		return light_height
 	set(value):
 		light_height = clamp(value,MIN_LIGHT_WIDTH_OR_HEIGHT,MAX_LIGHT_WIDTH_OR_HEIGHT)
-		generate_grid()
-@export_tool_button("Generate Grid") var generate_grid_action = generate_grid
-@export var activated_count: int
-
+		_generate_grid()
+@export_tool_button("Generate Grid") var generate_grid_action = _generate_grid
+@export var activated_count: int = 0:
+	get:
+		return activated_count
+	set(value):
+		activated_count = clamp(value,0,light_count)
+		_generate_grid()
 var light_count: int:
 	get:
 		return light_width * light_height
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body
+	_generate_grid()
 
 
-func generate_grid() -> void:
+func _generate_grid() -> void:
 	get_children().map(func(child) : child.queue_free())
 	for i in range(light_count):
 		var light : LightGridLight = light_scene.instantiate()
 		add_child(light)
+		light.set_state(LightGridLight.LightStates.LIGHT_ON if i < activated_count else LightGridLight.LightStates.LIGHT_OFF)
 		@warning_ignore("integer_division")
 		light.position = BASE_LIGHT_POS + Vector3(
 			(i % light_width) * LIGHT_SPACING,
