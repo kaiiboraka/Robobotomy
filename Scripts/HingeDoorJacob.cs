bf587747c82bc55430e2ec6eca94ea8f635d73c2
Jacob <jmemmott13@gmail.com>
2026-01-24 10:37:41 -0700

    get changes from kaiiboraka/Robobotomy

--- Scripts/HingeDoorJacob.cs
+++ Scripts/HingeDoorJacob.cs
@@ -4,34 +4,35 @@ using System;
 [Tool]
 public partial class HingeDoorJacob : Node3D
 {
-	[Export]
-	public float doorHeight;
-	[Export]
-	public float doorRadius;
-	[Export]
-	private MeshInstance3D hingeDoorMesh;
-	[Export]
-	private CollisionShape3D doorCollisionShape;
-	
-	private void _UpdateHingeDoorGeometry()
-	{
-		var doorMesh = hingeDoorMesh.Mesh as CylinderMesh;
-		doorMesh.Height = doorHeight;
-		doorMesh.TopRadius = doorRadius;
-		doorMesh.BottomRadius = doorRadius;
-		hingeDoorMesh.Position = new Vector3(0, doorHeight/2, 0);
-		
-		var collisionMesh = doorCollisionShape.Shape as CylinderShape3D;
-		collisionMesh.Height = doorHeight;
-		collisionMesh.Radius = doorRadius;
-		doorCollisionShape.Position = new Vector3(0, doorHeight/2, 0);
-	}
-	public override void _Ready()
-	{		
-		_UpdateHingeDoorGeometry();
-	}
-	public override void _Process(double delta) {
-		_UpdateHingeDoorGeometry();
-		//this.ApplyCentralForce(new Vector3(1, 0, 0));
-	}
+    [Export]
+    public float doorHeight;
+    [Export]
+    public float doorRadius;
+    [Export]
+    private MeshInstance3D hingeDoorMesh;
+    [Export]
+    private CollisionShape3D doorCollisionShape;
+
+    private void _UpdateHingeDoorGeometry()
+    {
+        var doorMesh = hingeDoorMesh.Mesh as CylinderMesh;
+        doorMesh.Height = doorHeight;
+        doorMesh.TopRadius = doorRadius;
+        doorMesh.BottomRadius = doorRadius;
+        hingeDoorMesh.Position = new Vector3(0, doorHeight / 2, 0);
+
+        var collisionMesh = doorCollisionShape.Shape as CylinderShape3D;
+        collisionMesh.Height = doorHeight;
+        collisionMesh.Radius = doorRadius;
+        doorCollisionShape.Position = new Vector3(0, doorHeight / 2, 0);
+    }
+    public override void _Ready()
+    {
+        _UpdateHingeDoorGeometry();
+    }
+    public override void _Process(double delta)
+    {
+        _UpdateHingeDoorGeometry();
+        //this.ApplyCentralForce(new Vector3(1, 0, 0));
+    }
 }
