12220c1075c9946e492494743199494a72d7ed7d
Jacob <jmemmott13@gmail.com>
2026-01-24 11:57:17 -0700

    add: hinge door takes in a mesh

--- Scripts/HingeDoorJacob.cs
+++ Scripts/HingeDoorJacob.cs
@@ -5,34 +5,78 @@ using System;
 public partial class HingeDoorJacob : Node3D
 {
     [Export]
-    public float doorHeight;
+    public float DoorHeight
+    {
+        get => DoorHeight; set
+        {
+            _UpdateHingeDoorGeometry();
+        }
+    }
     [Export]
-    public float doorRadius;
+    public float DoorRadius { get; set; }
     [Export]
-    private MeshInstance3D hingeDoorMesh;
+    public float UpperAngle { get; set; }
+    [Export]
+    public float LowerAngle { get; set; }
+
+    private MeshInstance3D _hingeDoorMesh;
+
+    private Mesh doorMesh;
+    [Export]
+    public Mesh DoorMesh
+    {
+        get => doorMesh;
+        set
+        {
+            doorMesh = value;
+            if (_hingeDoorMesh != null)
+            {
+                _UpdateHingeDoorGeometry();
+            }
+        }
+    }
+
     [Export]
     private CollisionShape3D doorCollisionShape;
 
     private void _UpdateHingeDoorGeometry()
     {
-        var doorMesh = hingeDoorMesh.Mesh as CylinderMesh;
-        doorMesh.Height = doorHeight;
-        doorMesh.TopRadius = doorRadius;
-        doorMesh.BottomRadius = doorRadius;
-        hingeDoorMesh.Position = new Vector3(0, doorHeight / 2, 0);
+        if (_hingeDoorMesh == null)
+        {
+            GD.PrintErr("No Hinge Door Instance set!");
+            return;
+        }
+        _hingeDoorMesh.Mesh = doorMesh;
 
         var collisionMesh = doorCollisionShape.Shape as CylinderShape3D;
-        collisionMesh.Height = doorHeight;
-        collisionMesh.Radius = doorRadius;
-        doorCollisionShape.Position = new Vector3(0, doorHeight / 2, 0);
+        collisionMesh.Height = DoorHeight;
+        collisionMesh.Radius = DoorRadius;
+        doorCollisionShape.Position = new Vector3(0, DoorHeight / 2, 0);
     }
+
+    public override void _EnterTree()
+    {
+        if (!Engine.IsEditorHint()) return;
+        GetComponents();
+    }
+
     public override void _Ready()
     {
+        GetComponents();
+
         _UpdateHingeDoorGeometry();
     }
-    public override void _Process(double delta)
+
+    // public override void _Process(double delta)
+    // {
+    // 	_UpdateHingeDoorGeometry();
+    // 	//this.ApplyCentralForce(new Vector3(1, 0, 0));
+    // }
+
+    private void GetComponents()
     {
-        _UpdateHingeDoorGeometry();
-        //this.ApplyCentralForce(new Vector3(1, 0, 0));
+        GD.Print("Getting Components");
+
+        _hingeDoorMesh ??= GetNode<MeshInstance3D>("%MeshInstance3D");
     }
 }
