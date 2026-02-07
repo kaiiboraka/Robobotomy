620ec1611051895e0c779975204443526e573b5c
Jacob <jmemmott13@gmail.com>
2026-02-07 11:41:54 -0700

    rollback: restarted Hinge since something in the script was breaking the scene

---
+++ Scripts/Hinge.cs
@@ -0,0 +1,71 @@
+using Godot;
+using System;
+// Hinge Door Specs:
+// this is a Hinge object
+// the level designer can do the following:
+	// attach a mesh that will rotate around the Hinge
+	// define angle constraints (display debug lines in viewport?)
+	// set the size of the hinge?
+[Tool]
+public partial class Hinge : Node3D
+{
+	[Export]
+	public float UpperAngle { get => upperAngle;  set {
+			upperAngle = value;
+		}
+	}
+	private float upperAngle;
+	[Export]
+	public float LowerAngle { get => lowerAngle; set
+		{
+			lowerAngle = value;
+		}
+	}
+	private float lowerAngle;
+	[Export]
+	public float ArmLength { get => armLength; set
+		{
+			armLength = value;
+		}
+	}
+	private float armLength;
+	[Export]
+	public float ArmWidth { get => armWidth; set
+		{
+			armWidth = value;
+		}
+	}
+	private float armWidth;
+	[Export]
+	public float Depth { get => depth; set
+		{
+			depth = value;
+		}
+	}
+	private float depth;
+	[Export]
+	public float BaseRadius { get => baseRadius; set
+		{
+			baseRadius = value;
+		}
+	}
+	private float baseRadius;
+
+	[Export]
+	public Mesh DoorMesh { get => doorMesh; set
+		{
+			doorMesh = value;
+		}
+	}
+	private Mesh doorMesh;	
+	
+	private MeshInstance3D _hingeDoorMesh;
+	private CollisionShape3D doorCollisionShape;
+
+
+	public override void _Ready()
+	{
+		GD.Print(doorMesh);
+	}
+
+}
