63d0805022aeb94987fbe986c0b3ee6b5b62b228
TheNewGrant <masoncgrant@gmail.com>
2026-03-21 11:06:06 -0600

    Add speed variables

--- Scripts/Nameless/character.gd
+++ Scripts/Nameless/character.gd
@@ -1,6 +1,12 @@
 extends CharacterBody3D
 class_name Nameless
 
-@export var speed := 10.0
 @export var gravity: int = 50
 @export var jump_impulse := 15
+@export var handicapped_jump := 13
+
+@export var speed := 10
+@export var handicap_speed := 8
+@export var major_handicap := 5
+@export var pull_speed := 5
+@export var push_speed := 5
