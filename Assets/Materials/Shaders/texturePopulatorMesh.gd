61ac07239a1edb884b66c413d617152164e70f56
bobaisbad <legomaster1007@gmail.com>
2026-03-31 13:12:09 -0600

    Writing texturePopulator.gd

--- Assets/Materials/Shaders/texturePopulatorMesh.gd
+++ Assets/Materials/Shaders/texturePopulatorMesh.gd
@@ -1,61 +1,8 @@
-extends Node3D
-#extends MeshInstance3D
-#extends GeometryInstance3D
-
-var shader: Shader = preload("res://Assets/Materials/Shaders/Wood/woodShader.tres")
-
-func populateShader(dir: String) -> ShaderMaterial:
-	var path := "res://Assets/Materials/Shaders/" + dir
-	var folder := DirAccess.open(path)
-	
-	var params
-	var mat := ShaderMaterial.new()
-	var baseColor
-	var height
-	var metallic
-	var normal
-	var roughness
-	
-	var i := 0
-	
-	for file in folder.get_files():
-		if i == 0: params = FileAccess.open(path + "/" + file, FileAccess.READ)
-		elif i == 1: mat.shader = load(path + "/" + file)
-		elif i == 2: baseColor = load(path + "/" + file)
-		elif i == 3: height = load(path + "/" + file)
-		elif i == 4: metallic = load(path + "/" + file)
-		elif i == 5: normal = load(path + "/" + file)
-		elif i == 6: roughness = load(path + "/" + file)
-		i += 1
-		
-	mat.set_shader_parameter("Base_color", baseColor)
-	mat.set_shader_parameter("Metallic", metallic)
-	mat.set_shader_parameter("Edge_Map", normal)
-	mat.set_shader_parameter("Roughness", roughness)
-	
-	var hue
-	var sat
-	var val
-	i = 0
-	
-	while not params.eof_reached():
-		var line = params.get_line()
-		if i == 0: hue = float(line)
-		elif i == 1: sat = float(line)
-		elif i == 2: val = float(line)
-		elif i > 2: break
-		i += 1
-		
-	mat.set_shader_parameter("Hue", hue)
-	mat.set_shader_parameter("Saturation", sat)
-	mat.set_shader_parameter("Value", val)
-	
-	return mat
+#extends Node
+extends MeshInstance3D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	var mesh_instance: GeometryInstance3D = $MeshInstance3D
-	
 	# Grab all the right shader and params files to populate
 	var parentDir := "res://Assets/Materials/Shaders/"
 	#var dirs = ["Wood", "Metal", "Concrete"]
