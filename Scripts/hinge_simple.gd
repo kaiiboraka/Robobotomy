0d1053194f1cda15e9919c588c36ec5c9001e245
Chase Odom <chasemichaelodom@gmail.com>
2026-04-03 22:27:33 -0600

    Fixed references to `HingeDoor_jacob.tscn` -> `hinge_simple.tscn`
    - Replaced broken references to non-existent `HingeDoor_jacob.tscn` with `hinge_simple.tscn`
    - Updated classname of `hinge_simple.tscn`: `hingeSimple` -> `HingeSimple`

--- Scripts/hinge_simple.gd
+++ Scripts/hinge_simple.gd
@@ -1,17 +1,14 @@
 @tool
-class_name hingeSimple extends Node3D;
+class_name HingeSimple
+extends Node3D
 
 @export_group("Hinge Parameters")
-@export var lower_angle: float = 0;
-@export var upper_angle: float = 0;
+@export var lower_angle: float = 0
+@export var upper_angle: float = 0
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	var hinge: HingeJoint3D = get_node("%HingeJoint3D")
 	hinge.set_param(HingeJoint3D.Param.PARAM_LIMIT_UPPER, upper_angle)
 	hinge.set_param(HingeJoint3D.Param.PARAM_LIMIT_LOWER, lower_angle)
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
