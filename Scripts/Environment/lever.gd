664ab5a9477faae361b773ef6f2a17ee7b8c4a15
bsharplydian <jmemmott13@gmail.com>
2026-07-29 23:05:00 -0600

    add lever script

---
+++ Scripts/Environment/lever.gd
@@ -0,0 +1,43 @@
+extends Node3D
+
+@export var activation_targets: Array[Node] = [];
+var player_in_range: bool;
+var _cached_player: Player;
+var player: Player :
+	get:
+		if not is_instance_valid(_cached_player):
+			_cached_player = Player.instance;
+		return _cached_player;
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	player= Player.instance;
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+	
+func _input(event: InputEvent) -> void:
+	if(!event.is_action_pressed("Player_Interact")):
+		return;
+	if(not player_in_range):
+		return;
+	if(player.l_arm.is_detached or player.r_arm.is_detached):
+		return;
+	on_activate();
+		
+func on_activate() -> void:
+	for target in activation_targets:
+		if target and target.has_method("on_activated"):
+			target.on_activated();
+		elif target and target.has_method("on_button_activated"):
+			target.on_button_activated();
+	
+func _on_area_3d_body_entered(body: Node3D) -> void:
+	if(body is Player):
+		# don't need to check type because the arms have their own collision layer
+		player_in_range = true;
+
+func _on_area_3d_body_exited(body: Node3D) -> void:
+	if(body is Player):
+		player_in_range = false;
